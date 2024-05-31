ab6d6117fef0acf81e556c848f1b737c20474b38
marioapn3 <mario.aprilnino27@gmail.com>
2024-05-31 10:50:31 +0700

    Edit File Service

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -1,20 +1,21 @@
 <?php
+
 namespace App\Services;
+
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
+
 class FileService
 {
     public function uploadFile($file)
     {
         $fileName = Str::random(60);
         $extension = $file->getClientOriginalExtension();
-        $path = date('Y') . '/' . date('m') . '/' . date('d');
-        $pathName = '/storage/file/' . $path . '/' . $fileName . '.' . $extension;
+        $pathName = '/storage/file/' . $fileName . '.' . $extension;
 
-        Storage::put('/public/file/' . $path . '/' . $fileName . '.' . $extension, File::get($file));
+        Storage::put('/public/file/' . $fileName . '.' . $extension, File::get($file));
 
         return  $pathName;
     }
 }
-
